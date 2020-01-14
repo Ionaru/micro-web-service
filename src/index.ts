90959ea74c68b895d03c891a2fb548d85b1196fd
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2020-01-14 18:50:40 +0100

    Expose custom interfaces

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as bodyParser from 'body-parser';
 import * as compression from 'compression';
 import * as express from 'express';
 
-export { ServiceController } from './controllers/service.controller';
+export { ServiceController, IOption, IRoute } from './controllers/service.controller';
 export { BaseRouter } from './routers/base.router';
 export { RequestLogger } from './loggers/request.logger';
 export { NextFunction, Request, RequestHandler, Response, Router } from 'express';
